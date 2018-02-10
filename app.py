7805e3dad386806d474a6b71f38b6b0f5259206e
Thomas Walker <thwalker@vt.edu>
2018-02-09 22:27:26 -0500

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 redis = Redis(host="redis", port=6379)
-@app_route('/')
+@app.route('/')
 def hello():
     redis.incr('hits')
     return 'Hello Docker Book reader!  I have been seen (0) times'.format(redis.get('hits'))
